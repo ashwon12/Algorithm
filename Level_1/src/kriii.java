fa1e87098d7cff791693738c734b00fbe382d348
김재원 <ahswon12@naver.com>
2020-07-28 01:47:46 +0900

    Create kriii.java

---
+++ Level_1/src/kriii.java
@@ -0,0 +1,11 @@
+
+public class kriii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}

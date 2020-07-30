ac55ca0ade1a77220633dcdde9e4e158041e5d26
김재원 <ahswon12@naver.com>
2020-07-31 01:00:42 +0900

    10950
    백준 10950

--- Level_3/src/plus_1.java
+++ Level_3/src/plus_1.java
@@ -5,7 +5,10 @@ public class plus_1 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		int T=0,A=0,B=0;
+		int T=0;
+		int A=0;
+		int B=0;
+		
 		
 		T=scan.nextInt();
 		for(int i=0; i<T; i++) {

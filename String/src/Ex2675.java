e3d8bdd40ee066d7b1dc323353b987d978916e4f
김재원 <ahswon12@naver.com>
2020-08-20 22:17:57 +0900

    문자열 반복

---
+++ String/src/Ex2675.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex2675 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T =scan.nextInt();
+
+		for(int i=0; i<T; i++) {
+			int R =scan.nextInt();
+			String S =scan.next();
+			for(int j=0; j<S.length(); j++) {
+				for(int k=0; k<R; k++) {
+					System.out.print(S.charAt(k));
+				}
+			
+			}
+			System.out.println();
+
+		}
+	}
+
+}

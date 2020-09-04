0a4121af7766cece25ba4bc4470bccc0edc85433
김재원 <ashwon12@naver.com>
2020-09-04 23:24:47 +0900

    빠진 숫자 찾기

---
+++ Algorithm_class/src/homework_1.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class homework_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+		Scanner scan = new Scanner(System.in);
+		
+		int N=scan.nextInt();
+		int[] Array = new int[N];
+		int[] newArray = new int[N+1];
+		
+		for(int i=0; i<N; i++) {
+			Array[i]=(random.nextInt(N))+1; // Array에 1~N 사이의난수 입력받기
+			System.out.println(Array[i]);
+			newArray[i]=0;// newArray 0으로 초기화
+		}
+		
+		for(int i=0; i<N; i++) {
+			newArray[(Array[i])]=1;
+			System.out.println("newArray["+i+"]"+ newArray[i]);
+		}
+		
+		for(int i=1; i<=N;i++) {
+			if(newArray[i]==0) {
+				System.out.println("없는 숫자는"+i);
+			}
+		}
+	}
+
+}

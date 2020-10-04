3a2facf230ded2a0aca2dbbe12dc6554c79b4631
김재원 <ashwon12@naver.com>
2020-10-05 02:14:28 +0900

    Dynamic programming
    막대나누기 예제

---
+++ Algorithm_class/src/DP_example.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class DP_example {
+	
+	private static Scanner scan = new Scanner(System.in);
+	private static int N=4;
+	private static int[] maxSell = new int[N];
+	private static int[] p = {5,10,6,8};
+
+	public static void main(String[] args) {
+	
+		int maxprice=cutRod_DC(p,N-1);
+		System.out.println(maxprice);
+	}
+	
+	private static int cutRod_DC(int[] p, int n) {
+		if(n ==0) return 0;
+		else {
+			for(int j=1; j<=n; j++) {
+				maxSell[0]=0;
+				int maxVal=0;
+				for(int k=1; k<=j; k++) {
+					maxVal=MAX(maxVal,p[k]+maxSell[j-k]);
+					maxSell[j]=maxVal;
+				}	
+			}
+			return maxSell[n];
+		}		
+	}
+
+	private static int MAX(int j, int i) {
+		if(i>j) return i;
+		else return j;
+	}
+
+
+
+}

9e2ec0a4097df8d4f09067ff3c99575454dbda4b
김재원 <ashwon12@naver.com>
2021-01-25 02:12:17 +0900

    [반복문] For

---
+++ Base/Ex11021.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex11021 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int T=scan.nextInt();
+		
+		
+		for(int i=1; i<=T; i++) {
+			int A =scan.nextInt();
+			int B=scan.nextInt();
+			
+			System.out.println("Case #"+i+": "+(A+B));
+		}
+	}
+
+}

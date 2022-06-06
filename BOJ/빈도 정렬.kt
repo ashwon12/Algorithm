77a78f7551b624e2fe19484907e26230c4620441
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-06 16:32:37 +0900

    Create 빈도 정렬.kt

---
+++ BOJ/빈도 정렬.kt
@@ -0,0 +1,12 @@
+fun main(){
+    readln().split(" ").map { it.toInt()}
+    val numbers = readln().split(" ").map { it.toInt() }
+    val map = mutableMapOf<Int,Int>()
+
+    numbers.forEach{ map[it] = (map[it]?:0).plus(1) }
+
+    val frequencyComparator = compareBy<Int> { map[it] }.reversed()
+    val indexComparator = frequencyComparator.thenBy{numbers.indexOf(it)}
+
+    println(numbers.sortedWith(indexComparator).joinToString(" "))
+}
